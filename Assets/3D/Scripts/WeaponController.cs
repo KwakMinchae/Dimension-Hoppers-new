446ec0992857475a6ac23e80712b4f44d092ea08
Minchae Kwak <110402168+KwakMinchae@users.noreply.github.com>
2024-06-20 17:53:17 +0900

    3D Sword Swinging + Sound

---
+++ Assets/3D/Scripts/WeaponController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+   public GameObject Sword;
+   public bool CanAttack = true; 
+   public float AttackCoolDown = 1.0f; 
+   public AudioClip SwordAttackSound;
+
+   void Update()
+   {
+    if (Input.GetKey(KeyCode.LeftShift))
+    {
+        if (CanAttack)
+        {
+            SwordAttack();
+        }
+    }
+   }
+
+   public void SwordAttack()
+   {
+        CanAttack = false; 
+        Animator anim = Sword.GetComponent<Animator>();
+        anim.SetTrigger("Attack");
+        AudioSource ac = GetComponent<AudioSource>();
+        ac.PlayOneShot(SwordAttackSound);
+        StartCoroutine(ResetAttackCoolDown());
+
+   }
+
+   IEnumerator ResetAttackCoolDown()
+   {
+    yield return new WaitForSeconds(AttackCoolDown);
+    CanAttack = true; 
+   }
+}
