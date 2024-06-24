49a59017b3b8c189281d3c48d52c329541776e91
LamKelsey <e1310991@u.nus.edu>
2024-06-24 23:40:04 +0800

    Health added
    Can deduct health and destroy card in deck so that there will not be duplicate card issues

---
+++ Assets/2D/scripts/CardManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public PlayerManager playerManager;
+    public DeckControl deckControl;
+    int index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            index = Random.Range(0, deckControl.deck.Count-1);
+            playerManager.playerHealth -= deckControl.deck[index].CardAttackHealingAmount;
+            Debug.Log("HP: " + playerManager.playerHealth);
+            Debug.Log(deckControl.deck.Count);
+            deckControl.deck.RemoveAt(index);
+        }
+    }
+}
--- Assets/2D/scripts/PlayerManager.cs
+++ Assets/2D/scripts/PlayerManager.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public int playerHealth = 500;
+    public int manaAmmount = 10;
+
     // Start is called before the first frame update
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playerHealth <= 0)
+        {
+            Debug.Log("GameOver");
+        }
         
     }
 }
